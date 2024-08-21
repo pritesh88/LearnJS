9346366bcdf21577077e72cd880eec6f25786192
Pritesh Lad <84929988+pritesh88@users.noreply.github.com>
2024-08-21 19:02:30 +0530

    Add files via upload

---
+++ Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 144523
+let accountEmail = "pritesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState; 
+
+ // accountId = 2 //not allowed 
+
+/* 
+   Prefer not to use var ,because of issue in block
+*/
+ accoundEmail ="hc@hc.com"
+ accountPassword ="888"
+ accountCity = "delhi"
+
+
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
