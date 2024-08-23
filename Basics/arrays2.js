e7af9f6658a6058b6ccca7fef0755a62d2e401af
Pritesh Lad <84929988+pritesh88@users.noreply.github.com>
2024-08-23 17:25:11 +0530

    Create arrays2.js

---
+++ Basics/arrays2.js
@@ -0,0 +1,29 @@
+//const hashiras =["Sanemi","Mitsuri","tengen"]
+//const demons = ["Douma","Kokushibo","Akaza"]
+
+//hashiras.push(demons);
+
+// console.log(hashiras);
+// console.log(hashiras[3][1])
+
+// const allDs = hashiras.concat(demons);
+// console.log(allDs);
+
+// const allDs = [...hashiras, ...demons]
+// console.log(allDs);
+
+// const long_array =[1,2,3[4,5,6,],7,[6,7[4,5]]]
+
+// const sort_array = long_array.flat(Infinity)
+// console.log(sort_array);
+
+console.log(Array.isArray("Pritesh"));
+console.log(Array.from("Pritesh"));
+console.log(Array.from({name:"pritesh"})) //interesting
+
+let score1 =100
+let score2 =200
+let score3 =300
+
+console.log(Array.of(score1,score2,score3));
+
