a810c70b244b4a4b820712cc8c00f1050b91d8fe
Pritesh Lad <84929988+pritesh88@users.noreply.github.com>
2024-09-09 04:46:32 +0000

    change

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const puppeteer = require('puppeteer');
 
 (async() => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
     await page.goto('https://en.wikipedia.org/wiki/Snail');
     await page.screenshot({path:'snail.png'});
+   //     await browser.waitForTarget(() => false);
 
+   const result = await page.evaluate(() =>{
+      let headingFromWeb = document.querySelectorAll(".mw-headline")
+      const headingList = [...headingFromWeb]
+      return headingList.map(h => h.innerText);
+
+   });
+
+   console.log(result);
     await browser.close();
 
 })();
